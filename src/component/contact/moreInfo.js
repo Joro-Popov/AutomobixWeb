a8d5a74e73246cef1adecad690498dbb5f1dc58e
Georgi Popov <popov937@abv.bg>
2019-11-30 15:43:42 +0200

    Working on contact page

---
+++ src/component/contact/moreInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MoreInfo extends Component {
+    render() {
+        return(
+        <div className="moreInfo">
+           <div>More contacts</div>
+        </div>
+        )
+    }
+}
