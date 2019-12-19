92d98f894f907e40a0c3279efa2d2783a394ea54
Georgi Popov <popov937@abv.bg>
2019-12-20 00:17:01 +0200

    Working on unauthorized routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Navigation from './component/common/navigation';
 import Footer from './component/common/footer';
 import Home from './containers/home';
@@ -9,9 +9,34 @@ import Contact from './containers/contact';
 import Authentication from './containers/authentication';
 import Details from './containers/details';
 import RepairDetails from './component/details/serviceDetails';
+import { thisExpression } from '@babel/types';
 
-export default class App extends Component {
+class App extends Component {
+  PrivateRoute = ({component: Component, authed, ...rest}) => {
+    return (
+      <Route
+        {...rest}
+        render={(props) => authed === true
+          ? <Component {...props} />
+          : <Redirect to={{pathname: '/authentication', state: {from: props.location}}} />}
+      />
+    )
+  }
+
+  PublicRoute = ({component: Component, authed, ...rest}) => {
+    return (
+      <Route
+        {...rest}
+        render={(props) => authed === true
+          ? <Redirect to={{pathname: '/', state: {from: props.location}}} />
+          : <Component {...props} />}
+      />
+    )
+  }
+
   render() {
+    const { isAuthenticated } = this.props;
+
     return (
       <Router>
         <div className="App">
@@ -19,12 +44,18 @@ export default class App extends Component {
             <Route exact path='/' component={Home} />
             <Route path='/about' component={About} />
             <Route path='/contact' component={Contact} />
-            <Route path='/authentication' component={Authentication} />
-            <Route exact path='/repairs/:id' component={Details} />
-            <Route path='/repairs/details/:id' component={RepairDetails} />
+            <this.PublicRoute authed={isAuthenticated} path='/authentication' component={Authentication} />
+            <this.PrivateRoute authed={isAuthenticated} exact path='/repairs/:id' component={Details} />
+            <this.PrivateRoute authed={isAuthenticated} path='/repairs/details/:id' component={RepairDetails} />
           <Footer />
         </div>
       </Router>
     );
   }
 }
+
+const mapStateToProps = state => ({ 
+  isAuthenticated: state.authReducer.isAuthenticated, 
+});
+
+export default connect(mapStateToProps, null)(App)  
